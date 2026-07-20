18c230bd6dcc4a76cad366cf568babde81ccea54
Newfund88 <131372177+Newfund88@users.noreply.github.com>
2026-07-20 09:28:44 +0800

    release: update appsift homebrew cask version to 1.0.4

--- homebrew/appsift.rb
+++ homebrew/appsift.rb
@@ -2,8 +2,8 @@ cask "appsift" do
   # Set to the published ZIP checksum after each release. Current customer
   # artifacts are explicitly self-signed; a future Developer ID migration
   # rewrites this URL to the notarized artifact name.
-  version "1.0.0"
-  sha256 :no_check
+  version "1.0.4"
+  sha256 "0ac19b0f70a455ae8bc845ed4a5d676d4d1a6400243ee300861b3241ffdad0d3"
 
   url "https://github.com/GravityPoet/AppSift/releases/download/v#{version}/AppSift-#{version}-self-signed.zip"
   name "AppSift"
